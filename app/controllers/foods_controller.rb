bfef6fc9b91a63835f7f73760b89597a74c3fdd5
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-14 10:02:49 -0700

    Update search results and specify params to be in quotes. PASSING TEST

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,14 +1,16 @@
 class FoodsController < ApplicationController
   def index
-    keyword = params[:q]
+    keyword = "\"#{params[:q]}\""
     response = Faraday.get('https://api.nal.usda.gov/fdc/v1/foods/search') do |f|
       f.params["api_key"] = ENV['USDA_API_KEY']
       f.params["query"] = keyword
       f.params["pageSize"] = 10
+      f.params["sortBy"] = "ingredients.keyword"
     end
 
     json = JSON.parse(response.body, symbolize_names: true)
 
     @foods = json[:foods]
+    @total_search_results = json[:totalHits]
   end
 end
